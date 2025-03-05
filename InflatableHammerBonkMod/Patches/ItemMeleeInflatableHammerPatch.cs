8667d0f61b972b7ec38bd97a9dd1fd20ad637190
ColtG5 <colton.gowans@ucalgary.ca>
2025-03-04 17:58:12 -0700

    Add project files.

---
+++ InflatableHammerBonkMod/Patches/ItemMeleeInflatableHammerPatch.cs
@@ -0,0 +1,47 @@
+﻿using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace InflatableHammerBonkMod.Patches
+{
+	[HarmonyPatch(typeof(ItemMelee))]
+	class ItemMeleeInflatableHammerPatch
+	{
+		private static InflatableHammerBonkMod Instance => InflatableHammerBonkMod.instance;
+		private static ManualLogSource Logger => Instance.logger;
+
+		internal static List<AudioClip> newHitSound;
+		internal static AssetBundle Bundle;
+
+		[HarmonyPatch("Start")]
+		[HarmonyPostfix]
+		static void PatchItemMeleeStart(ItemMelee __instance)
+		{
+			if (__instance.gameObject.name.Contains("nflatable"))
+			{
+				string folderPath = Instance.Info.Location;
+				folderPath = folderPath.TrimEnd("InflatableHammerBonkMod.dll".ToCharArray());
+				Bundle = AssetBundle.LoadFromFile(folderPath + "bonk");
+
+				if (Bundle != null)
+				{
+					newHitSound = Bundle.LoadAllAssets<AudioClip>().ToList();
+					Logger.LogInfo("Asset bundle loaded!");
+				}
+				else
+				{
+					Logger.LogError("Failed to load asset bundle!");
+				}
+
+				__instance.soundHit.Sounds = newHitSound.ToArray();
+				__instance.soundHit.Volume = 2.0f;
+			}
+		}
+
+	}
+}
---
+++ InflatableHammerBonkMod/Plugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InflatableHammerBonkMod
+{
+	[BepInPlugin(modGUID, modName, modVersion)]
+	public class InflatableHammerBonkMod : BaseUnityPlugin
+	{
+		private const string modGUID = "ColtG5.InflatableHammerBonk";
+		private const string modName = "InflatableHammerBonk";
+		private const string modVersion = "1.0.0";
+
+		private readonly Harmony harmony = new Harmony(modGUID);
+
+		internal static InflatableHammerBonkMod instance;
+		internal ManualLogSource logger;
+
+		private void Awake()
+		{
+			if (instance == null)
+			{
+				instance = this;
+			}
+			logger = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+			logger.LogInfo("InflatableHammerBonkMod loaded!");
+
+			logger = Logger;
+			harmony.PatchAll();
+		}
+	}
+}
